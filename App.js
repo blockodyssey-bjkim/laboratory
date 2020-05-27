1f0993309c781c4915b6daadd1e9cb77799f2f21
billowycloud <bjkim0228@naver.com>
2020-05-27 19:32:01 +0900

    AppLoading 설정

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { Image } from "react-native";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+
+const cacheImages = (images) => {
+  if (typeof image === "string") {
+    return Image.prefetch(image);
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+};
+
+const cacheFonts = (fonts) => {
+  fonts.map((font) => Font.loadAsync(fonts));
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+    const images = cacheImages([
+      "http://pngimg.com/uploads/medical_mask/medical_mask_PNG41.png",
+      require("./assets/splash.png"),
+    ]);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+
+  const onFinish = () => setIsReady(true);
+
+  return isReady ? null : (
+    <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
